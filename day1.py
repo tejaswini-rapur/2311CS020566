11bbff64db3f7bf539e7d3a02ad7b52b40bf20ae
tejaswini-rapur <rapurtejaswini@gmail.com>
2025-01-07 19:47:09 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,19 @@
+# Declare variables of different data types
+
+# Integer
+my_integer = 10
+
+# Float
+my_float = 3.14
+
+# String
+my_string = "Hello, Jupyter!"
+
+# Boolean
+my_boolean = True
+
+# Print each variable and its type
+print("Value of my_integer:", my_integer, "| Type of my_integer:", type(my_integer))
+print("Value of my_float:", my_float, "| Type of my_float:", type(my_float))
+print("Value of my_string:", my_string, "| Type of my_string:", type(my_string))
+print("Value of my_boolean:", my_boolean, "| Type of my_boolean:", type(my_boolean))
