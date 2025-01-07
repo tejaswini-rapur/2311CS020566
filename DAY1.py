d2c86ec05f056f07bfc9e24e5e36c146a7381402
tejaswini-rapur <rapurtejaswini@gmail.com>
2025-01-07 19:41:41 +0530

    Add files via upload

---
+++ DAY1.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "01e9ef0f-03a5-4dab-9d72-1715a0799c72",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
